549afce479eac8f9c16805b300c77939e6975b98
Gilmar Horchulhak <89936389+GilmarHorchulhak@users.noreply.github.com>
2024-04-05 04:09:08 +0000

    3

--- variáveis.c
+++ variáveis.c
@@ -1,22 +1,19 @@
 #include <stdio.h>
 
-
-
 int main()
 {
     char c = 'a';
-    char n = 10;
     int x = 5;
     float f = 5.25;
     double d = 15.673;
     double y = 5.0e10;
-   
 
-    string texto = "Hello, world!";
-    
-  
+    int numeros[5] = {1, 2, 3, 4, 5};
+    for (size_t i = 0; i < 4; i++)
+    {
+        printf("%d \n", numeros[i]);
+    }
     printf("O valor é c =%c \n", c);
-    printf("O valor é n =%c \n", n);
     printf("O valor é x =%d \n", x);
     printf("O valor é f =%f \n", f);
     printf("O valor é d =%f \n", d);
