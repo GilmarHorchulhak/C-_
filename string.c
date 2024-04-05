b07b1652ae5fc7805ad114e6ad07bac3df0ab449
Gilmar Horchulhak <89936389+GilmarHorchulhak@users.noreply.github.com>
2024-04-05 03:50:01 +0000

    2

---
+++ string.c
@@ -0,0 +1,10 @@
+#include <iostream>
+#include <string>
+
+
+
+int main() {
+    string texto = "Hello, world!";
+    cout << texto << endl;  // Saída: Hello, world!
+    return 0;
+}
---
+++ variáveis.py
@@ -0,0 +1,29 @@
+#Inteiro (int):
+numero_inteiro = 10
+print(numero_inteiro)  # Saída: 10
+#Float:
+numero_decimal = 3.14
+print(numero_decimal)  # Saída: 3.14
+#Booleano (bool):
+verdadeiro = True
+falso = False
+print(verdadeiro)  # Saída: True
+print(falso)  # Saída: False
+#String (str):
+texto = "Olá, mundo!"
+print(texto)  # Saída: Olá, mundo!
+#Lista (list):
+lista = [1, 2, 3, 4]
+print(lista)  # Saída: [1, 2, 3, 4]
+#Tupla (tuple):
+tupla = (1, 2, 3)
+print(tupla)  # Saída: (1, 2, 3)
+#Conjunto (set):
+conjunto = {1, 2, 3}
+print(conjunto)  # Saída: {1, 2, 3}
+#Dicionário (dict):
+dicionario = {"nome": "João", "idade": 30}
+print(dicionario)  # Saída: {'nome': 'João', 'idade': 30}
+#NoneType (None):
+valor_nulo = None
+print(valor_nulo)  # Saída: None
